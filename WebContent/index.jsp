dc32eac1dd0752aead34f7447c2d8995ed09e1bb
kjb0722 <58472980+kjb0722@users.noreply.github.com>
2020-05-12 09:51:04 +0900

    ER 다이어그램 수정 DDL 수정

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -26,10 +27,19 @@
 		<div class="navbar">
 			<ul class="nav navbar-nav pull-right">
 				<li><a href="">회원가입</a></li>
-				<li><a href="">로그인</a></li>
-				<li><a href="">관리자</a></li>
+				<c:choose>
+					<c:when test="${sessionScope.id == null }">
+						<li><a href="${cp }/member/login.do">로그인</a></li>						
+					</c:when>
+					<c:otherwise>
+						<li><a href="${cp }/member/logout.do">로그아웃</a></li>
+					</c:otherwise>
+				</c:choose>
+				<c:if test="${sessionScope.rating == 99 }">
+					<li><a href="/admin/main.do">관리자</a></li>				
+				</c:if>
 			</ul>
-			<a href="${cp }/main.do"><img src="img/img1.jpg" alt="Logo"></a>
+			<a href="${cp }/main.do"><img src="img/logo.png" alt="Logo"></a>
 		</div>
 		<nav class="navbar navbar-default">
 			<div class="container">
@@ -58,46 +68,69 @@
 	</header>
 
 	<section class="container">
-		<div id="carousel-example-generic" class="carousel slide"
-			data-ride="carousel">
-			<ol class="carousel-indicators">
-				<li data-target="#carousel-example-generic" data-slide-to="0"
-					class="active"></li>
-				<li data-target="#carousel-example-generic" data-slide-to="1"
-					class=""></li>
-				<li data-target="#carousel-example-generic" data-slide-to="2"
-					class=""></li>
-			</ol>
-			<div class="carousel-inner" role="listbox">
-				<div class="item active">
-					<img data-src="holder.js/1140x500/auto/#777:#555/text:First slide"
-						alt="First slide [1140x500]" src="img/img1.jpg"
-						data-holder-rendered="true">
+		<c:choose>
+			<c:when test="${page == null }">
+				<div id="carousel-example-generic" class="carousel slide"
+					data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#carousel-example-generic" data-slide-to="0"
+							class="active"></li>
+						<li data-target="#carousel-example-generic" data-slide-to="1"
+							class=""></li>
+						<li data-target="#carousel-example-generic" data-slide-to="2"
+							class=""></li>
+					</ol>
+					<div class="carousel-inner" role="listbox">
+						<div class="item active">
+							<img
+								data-src="holder.js/1140x500/auto/#777:#555/text:First slide"
+								alt="First slide [1140x500]" src="img/img1.jpg"
+								data-holder-rendered="true">
+						</div>
+						<div class="item">
+							<img
+								data-src="holder.js/1140x500/auto/#666:#444/text:Second slide"
+								alt="Second slide [1140x500]" src="img/img2.png"
+								data-holder-rendered="true">
+						</div>
+						<div class="item">
+							<img
+								data-src="holder.js/1140x500/auto/#555:#333/text:Third slide"
+								alt="Third slide [1140x500]" src="img/img3.jpg"
+								data-holder-rendered="true">
+						</div>
+					</div>
+					<a class="left carousel-control" href="#carousel-example-generic"
+						role="button" data-slide="prev"> <span
+						class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a> <a class="right carousel-control" href="#carousel-example-generic"
+						role="button" data-slide="next"> <span
+						class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
 				</div>
-				<div class="item">
-					<img data-src="holder.js/1140x500/auto/#666:#444/text:Second slide"
-						alt="Second slide [1140x500]" src="img/img2.png"
-						data-holder-rendered="true">
-				</div>
-				<div class="item">
-					<img data-src="holder.js/1140x500/auto/#555:#333/text:Third slide"
-						alt="Third slide [1140x500]" src="img/img3.jpg"
-						data-holder-rendered="true">
-				</div>
-			</div>
-			<a class="left carousel-control" href="#carousel-example-generic"
-				role="button" data-slide="prev"> <span
-				class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a> <a class="right carousel-control" href="#carousel-example-generic"
-				role="button" data-slide="next"> <span
-				class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
-		</div>
+			</c:when>	
+			<c:otherwise>
+				<jsp:include page="${page }"></jsp:include>
+			</c:otherwise>	
+		</c:choose>
 	</section>
 	<footer class="container">
-		
+		<div class="panel panel-success">
+			<div class="panel-heading">
+				<h3 class="panel-title">마켓 홀리쒯</h3>
+			</div>
+			<div class="panel-body">
+				<h3>제작자</h3>
+				<ul>
+					<li>권준범</li>
+					<li>임다은</li>
+					<li>전인표</li>
+					<li>전효진</li>
+				</ul>
+			</div>
+		</div>
 	</footer>
 	<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 	<script src="${cp }/bootstrap/js/bootstrap.min.js"></script>
---
+++ WebContent/semiProject.sql
@@ -0,0 +1,319 @@
+
+/* Drop Tables */
+
+DROP TABLE order_product CASCADE CONSTRAINTS;
+DROP TABLE qna CASCADE CONSTRAINTS;
+DROP TABLE review CASCADE CONSTRAINTS;
+DROP TABLE sale CASCADE CONSTRAINTS;
+DROP TABLE product CASCADE CONSTRAINTS;
+DROP TABLE cart CASCADE CONSTRAINTS;
+DROP TABLE category CASCADE CONSTRAINTS;
+DROP TABLE common CASCADE CONSTRAINTS;
+DROP TABLE refund_history CASCADE CONSTRAINTS;
+DROP TABLE orders CASCADE CONSTRAINTS;
+DROP TABLE point_history CASCADE CONSTRAINTS;
+DROP TABLE member CASCADE CONSTRAINTS;
+
+
+
+
+/* Create Tables */
+
+CREATE TABLE cart
+(
+	cartnum number NOT NULL,
+	id varchar2(25) NOT NULL,
+	pnum number NOT NULL,
+	EA number NOT NULL,
+	PRIMARY KEY (cartnum)
+);
+
+DROP SEQUENCE seq_cart_num;
+CREATE SEQUENCE seq_cart_num;
+
+CREATE TABLE category
+(
+	cnum number NOT NULL,
+	type number,
+	name varchar2(30),
+	PRIMARY KEY (cnum)
+);
+
+DROP SEQUENCE seq_category_cnum;
+CREATE SEQUENCE seq_category_cnum;
+
+CREATE TABLE common
+(
+	type varchar2(40) UNIQUE,
+	conum number,
+	name varchar2(50)
+);
+
+
+CREATE TABLE member
+(
+	num number NOT NULL,
+	id varchar2(25),
+	pwd varchar2(25),
+	name varchar2(30),
+	rating number,
+	email varchar2(40),
+	birth varchar2(30),
+	phone varchar2(30),
+	-- 1:남자
+	-- 2:여자
+	gender number,
+	addr varchar2(50),
+	reg_date date,
+	-- y:삭제
+	-- n:미삭제
+	del_yn char,
+	del_date date,
+	point number,
+	PRIMARY KEY (num)
+);
+
+DROP SEQUENCE seq_member_num;
+CREATE SEQUENCE seq_member_num;
+
+CREATE TABLE orders
+(
+	onum number NOT NULL,
+	num number NOT NULL,
+	opnum number,
+	-- 1:상품 준비
+	-- 2:배송 준비
+	-- 3:배송 중
+	-- 4:배송 완료
+	-- 5:구매 완료
+	-- 6:주문 취소
+	status number,
+	-- 1:미결제
+	-- 2:결제
+	pay_yn number,
+	reg_date date,
+	end_date date,
+	id varchar2(25),
+	price number,
+	use_point number,
+	sale_price number,
+	-- 카드 결제
+	-- 무통장
+	pay_way number,
+	PRIMARY KEY (onum)
+);
+
+DROP SEQUENCE seq_orders_onum;
+CREATE SEQUENCE seq_orders_onum;
+
+CREATE TABLE order_product
+(
+	opnum number NOT NULL,
+	onum number NOT NULL,
+	pnum number NOT NULL,
+	pname varchar2(30),
+	EA number,
+	price number,
+	PRIMARY KEY (opnum)
+);
+
+DROP SEQUENCE seq_order_product_opnum;
+CREATE SEQUENCE seq_order_product_opnum;
+
+CREATE TABLE point_history
+(
+	ponum number NOT NULL,
+	num number NOT NULL,
+	id varchar2(35),
+	use_vol number,
+	reg_date date,
+	del_yn char,
+	PRIMARY KEY (ponum)
+);
+
+DROP SEQUENCE seq_point_history_ponum;
+CREATE SEQUENCE seq_point_history_ponum;
+
+CREATE TABLE product
+(
+	pnum number NOT NULL,
+	cnum number NOT NULL,
+	cartnum number NOT NULL,
+	name varchar2(40),
+	reg_date date,
+	del_yn char,
+	price number,
+	stock number,
+	type number,
+	orgimg varchar2(50),
+	saveimg varchar2(50),
+	PRIMARY KEY (pnum)
+);
+
+DROP SEQUENCE seq_product_pnum;
+CREATE SEQUENCE seq_product_pnum;
+
+CREATE TABLE qna
+(
+	pnum number NOT NULL,
+	num number NOT NULL,
+	rnum number,
+	id varchar2(40),
+	name varchar2(40),
+	title varchar2(50),
+	content varchar2(100),
+	ref number,
+	lev number,
+	step number,
+	regdate date,
+	del_yn char,
+	UNIQUE (pnum, num)
+);
+
+
+CREATE TABLE refund_history
+(
+	num number NOT NULL,
+	onum number NOT NULL,
+	reason varchar2(100),
+	reg_date date
+);
+
+
+CREATE TABLE review
+(
+	onum number NOT NULL,
+	pnum number NOT NULL,
+	num number NOT NULL,
+	rnum number,
+	id varchar2(40),
+	name varchar2(40),
+	title varchar2(50),
+	content varchar2(100),
+	regdate date,
+	del_yn char,
+	pwd varchar2(40),
+	UNIQUE (onum, pnum, num)
+);
+
+
+CREATE TABLE sale
+(
+	snum number NOT NULL,
+	pnum number NOT NULL,
+	percent number,
+	start_date date,
+	end_date date,
+	del_yn char,
+	PRIMARY KEY (snum)
+);
+
+DROP SEQUENCE seq_sale_snum;
+CREATE SEQUENCE seq_sale_snum;
+
+/* Create Foreign Keys */
+
+ALTER TABLE product
+	ADD FOREIGN KEY (cartnum)
+	REFERENCES cart (cartnum)
+;
+
+
+ALTER TABLE product
+	ADD FOREIGN KEY (cnum)
+	REFERENCES category (cnum)
+;
+
+
+ALTER TABLE orders
+	ADD FOREIGN KEY (num)
+	REFERENCES member (num)
+;
+
+
+ALTER TABLE point_history
+	ADD FOREIGN KEY (num)
+	REFERENCES member (num)
+;
+
+
+ALTER TABLE qna
+	ADD FOREIGN KEY (num)
+	REFERENCES member (num)
+;
+
+
+ALTER TABLE refund_history
+	ADD FOREIGN KEY (num)
+	REFERENCES member (num)
+;
+
+
+ALTER TABLE review
+	ADD FOREIGN KEY (num)
+	REFERENCES member (num)
+;
+
+
+ALTER TABLE order_product
+	ADD FOREIGN KEY (onum)
+	REFERENCES orders (onum)
+;
+
+
+ALTER TABLE refund_history
+	ADD FOREIGN KEY (onum)
+	REFERENCES orders (onum)
+;
+
+
+ALTER TABLE review
+	ADD FOREIGN KEY (onum)
+	REFERENCES orders (onum)
+;
+
+
+ALTER TABLE order_product
+	ADD FOREIGN KEY (pnum)
+	REFERENCES product (pnum)
+;
+
+
+ALTER TABLE qna
+	ADD FOREIGN KEY (pnum)
+	REFERENCES product (pnum)
+;
+
+
+ALTER TABLE review
+	ADD FOREIGN KEY (pnum)
+	REFERENCES product (pnum)
+;
+
+
+ALTER TABLE sale
+	ADD FOREIGN KEY (pnum)
+	REFERENCES product (pnum)
+;
+
+
+
+/* Comments */
+
+COMMENT ON COLUMN member.gender IS '1:남자
+2:여자';
+COMMENT ON COLUMN member.del_yn IS 'y:삭제
+n:미삭제';
+COMMENT ON COLUMN orders.status IS '1:상품 준비
+2:배송 준비
+3:배송 중
+4:배송 완료
+5:구매 완료
+6:주문 취소';
+COMMENT ON COLUMN orders.pay_yn IS '1:미결제
+2:결제';
+COMMENT ON COLUMN orders.pay_way IS '카드 결제
+무통장';
+
+
+
