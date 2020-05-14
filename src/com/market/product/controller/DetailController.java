b3fd290a22417c759fc15f727723179ec46c97c4
danny <ekdms348@gmail.com>
2020-05-14 15:52:46 +0900

    g

--- src/com/market/product/controller/DetailController.java
+++ src/com/market/product/controller/DetailController.java
@@ -1,15 +1,19 @@
 package com.market.product.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.market.product.dto.ProductDto;
+
 public class DetailController extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		int pnum=Integer.parseInt(req.getParameter("pnum"));
+		ArrayList<ProductDto>list=new ArrayList<ProductDto>();
 	}
 }
