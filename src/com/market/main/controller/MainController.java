997341f34b2eeeafa276d8c796ddb1ca45e1c03c
kjb0722 <58472980+kjb0722@users.noreply.github.com>
2020-05-14 11:15:28 +0900

    dfdf

--- src/com/market/main/controller/MainController.java
+++ src/com/market/main/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.market.main.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -9,13 +10,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.market.admin.dao.CategoryDao;
+import com.market.admin.dto.CategoryDto;
+
 @WebServlet("/main.do")
-public class MainController extends HttpServlet{
+public class MainController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		ServletContext app = req.getServletContext();
 		String cp = req.getContextPath();
 		app.setAttribute("cp", cp);
-		resp.sendRedirect(cp+"/index.jsp"); 
+		
+		//"전체 카테고리" 카테고리
+		CategoryDao dao = CategoryDao.getInstance();
+		ArrayList<CategoryDto> catList = dao.selList();
+		app.setAttribute("catList", catList);
+
+		//"전체 카테고리" 카테고리
+		ArrayList<CategoryDto> catTypeList = dao.selTypeList();
+		app.setAttribute("catTypeList", catTypeList);
+		
+		req.getRequestDispatcher("/index.jsp").forward(req, resp);
 	}
 }
