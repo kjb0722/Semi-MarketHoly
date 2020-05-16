486164d514f20b4b06421126c4a6f756dbedc8a1
kjb0722 <wnswnsqja@naver.com>
2020-05-17 05:11:26 +0900

    ER다이어그램 복구

---
+++ WebContent/DB/sequence.sql
@@ -0,0 +1,29 @@
+DROP SEQUENCE seq_review_num;
+CREATE SEQUENCE seq_review_num;
+
+DROP SEQUENCE seq_cart_num;
+CREATE SEQUENCE seq_cart_num;
+
+DROP SEQUENCE seq_category_cnum_type;
+CREATE SEQUENCE seq_category_cnum_type;
+
+DROP SEQUENCE seq_common_conum;
+CREATE SEQUENCE seq_common_conum;
+
+DROP SEQUENCE seq_member_num;
+CREATE SEQUENCE seq_member_num;
+
+DROP SEQUENCE seq_orders_onum;
+CREATE SEQUENCE seq_orders_onum;
+
+DROP SEQUENCE seq_order_product_opnum;
+CREATE SEQUENCE seq_order_product_opnum;
+
+DROP SEQUENCE seq_point_history_ponum;
+CREATE SEQUENCE seq_point_history_ponum;
+
+DROP SEQUENCE seq_product_pnum;
+CREATE SEQUENCE seq_product_pnum;
+
+DROP SEQUENCE seq_sale_snum;
+CREATE SEQUENCE seq_sale_snum;
