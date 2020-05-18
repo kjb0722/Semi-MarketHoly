2fabb8b723eeec0a224acbef1ac9f1f464d46866
kjb0722 <58472980+kjb0722@users.noreply.github.com>
2020-05-18 19:14:53 +0900

    관리자-qna 관리 게시판 생성

---
+++ WebContent/admin/qna.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<div class="container">
+	<div class="row">
+		<h3>QnA</h3>
+	</div>
+	<div class="row">
+		<div class="col-md-7 form-inline">
+			<span class="label label-success">검색 종류</span>
+			<select name="cat" class="form-control">
+				<option value="dd">상품명</option>
+			</select>
+			<input type="text" class="form-control" placeholder="검색어를 입력하세요" maxlength="30">
+			<input type="button" class="btn btn-lg btn-primary" value="검색">
+		</div>
+	</div>
+</div>
---
+++ src/com/market/admin/controller/QnaController.java
@@ -0,0 +1,17 @@
+package com.market.admin.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/qna.do")
+public class QnaController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.sendRedirect(req.getContextPath()+"/index.jsp?page=/admin/qna.jsp");
+	}
+}
